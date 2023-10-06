beaf455a01b677dbf60dc3aec5391b6b66cfd6ba
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-10-06 01:53:41 -0600

    Movement additions/fixes
    Preparing rolling into the movement and fixed the diagonal movement speed.

--- Capstone/Assets/Scripts/PlayerController.cs
+++ Capstone/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] SpriteRenderer spriterenderer;
+    [SerializeField] Animator animator;
     [SerializeField] float Speed;
+
+    [Header("Rolling")]
+    private bool CanRoll = true;
+    private bool IsRolling;
+    [SerializeField] private float RollingPower = 1.5f;
+    [SerializeField] private float RollingTime = 0.8f;
+    [SerializeField] private float RollingCooldown = 1.25f;
 
     Rigidbody2D RB;
 
     Vector2 Velocity = Vector2.zero;
+    //bool FaceRight = true;
 
     void Start() {
         RB = GetComponent<Rigidbody2D>();
@@ -16,14 +26,22 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update() {
+        if (IsRolling) return;
+
         Vector2 Direction = Vector2.zero;
         Direction.x = Input.GetAxis("Horizontal");
         Direction.y = Input.GetAxis("Vertical");
-
-        Debug.DrawRay(transform.position, Direction, Color.green);
+        Direction.Normalize();
 
         Velocity.x = Direction.x * Speed;
         Velocity.y = Direction.y * Speed;
+
+        // Rolling
+        if (Input.GetKeyDown(KeyCode.E) && CanRoll) {
+            animator.SetTrigger("Roll");
+            //SoundManager.Instance.PlaySound(RollSound);
+            StartCoroutine(Roll());
+        }
 
         // move character
         RB.velocity = Velocity;
@@ -31,5 +49,17 @@ public class PlayerController : MonoBehaviour {
         // Rotate character to face direction of movement
         //if (Velocity.x > 0 && !FaceRight) Flip();
         //if (Velocity.x < 0 && FaceRight) Flip();
+    }
+
+    IEnumerator Roll() {
+        CanRoll = false;
+        IsRolling = true;
+        Physics2D.IgnoreLayerCollision(3, 6, true);
+        Velocity.x += transform.localScale.x * RollingPower;
+        yield return new WaitForSeconds(RollingTime);
+        IsRolling = false;
+        yield return new WaitForSeconds(RollingCooldown);
+        Physics2D.IgnoreLayerCollision(3, 6, false);
+        CanRoll = true;
     }
 }
