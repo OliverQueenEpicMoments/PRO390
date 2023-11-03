e4fb76f1cbe98bd7ac0a2ba8d5e63fcef5e2aaa5
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-11-03 03:06:36 -0600

    Enemy AI
    Attempted to do enemy AI stuff to very little success.

--- Capstone/Assets/Scripts/AI/EnemyNavAI.cs
+++ Capstone/Assets/Scripts/AI/EnemyNavAI.cs
@@ -9,13 +9,15 @@ public class EnemyNavAI : MonoBehaviour {
     [SerializeField] Health EnemyHealth;
     [SerializeField] float Speed;
     [Header("AI")]
-    [SerializeField] Transform PlayerLocation;
+    [SerializeField] GameObject Player;
     [SerializeField] float EnemyDamage = 1;
     [SerializeField] float EnemyKnockback = 0;
 
     NavMeshAgent Agent;
     Rigidbody2D RB;
     Vector2 Velocity = Vector2.zero;
+    private bool AttackSwap = true;
+    private bool IsStunned = false;
 
     void Start() {
         RB = GetComponent<Rigidbody2D>();
@@ -28,22 +30,31 @@ public class EnemyNavAI : MonoBehaviour {
     }
 
     void Update() {
-        if (EnemyHealth.CurrentHealth <= 0) StartCoroutine(Death());
+        Vector2 Direction = Player.transform.position - transform.position;
+        Direction.Normalize();
+        Velocity = Direction * Speed;
 
-        if (EnemyHealth.CurrentHealth > 0) Agent.SetDestination(PlayerLocation.position);
-        Vector2 Direction = PlayerLocation.position - transform.position;
+        if (EnemyHealth.CurrentHealth <= 0) {
+            StartCoroutine(Death());
+            Agent.SetDestination(transform.position);
+        }
+
+        Debug.Log(Velocity.magnitude);
+        if (IsStunned) return;
+
+        Agent.SetDestination(Player.transform.position);
+
+        if (EnemyHealth.CurrentHealth > 0) Agent.SetDestination(Player.transform.position);
 
         //Velocity.x = Direction.x * Speed;
         //Velocity.y = Direction.y * Speed;
 
-        float Distance = (PlayerLocation.position - transform.position).magnitude;
-        if (Distance <= 4) {
-            // Attack
-            animator.SetTrigger("Attack1");
+        float Distance = (Player.transform.position - transform.position).magnitude;
+        if (Distance <= 4 && !IsStunned) {
+            StartCoroutine(Attack(AttackSwap));
+            StartCoroutine(Stun(0));
         }
 
-        //Direction.Normalize();
-        //Velocity = Direction * Speed;
 
         // Move character
         //RB.velocity = Velocity;
@@ -51,11 +62,37 @@ public class EnemyNavAI : MonoBehaviour {
         // Update the animator
         animator.SetFloat("Horizontal", Direction.x);
         animator.SetFloat("Vertical", Direction.y);
-        animator.SetFloat("Speed", Direction.sqrMagnitude);
+        animator.SetFloat("Speed", Velocity.magnitude);
+    }
+
+    IEnumerator Attack(bool chain) {
+        Vector2 Direction = (Player.transform.position - transform.position).normalized;
+        Vector2 Knockback = Direction * EnemyKnockback;
+
+        if (chain) {
+            animator.SetTrigger("Attack1");
+            yield return new WaitForSeconds(0.5f);
+            Player.GetComponent<Health>().TakeDamage(EnemyDamage, Knockback);
+            AttackSwap = false;
+            StartCoroutine(Stun(5));
+        } else {
+            animator.SetTrigger("Attack2");
+            yield return new WaitForSeconds(2f);
+            Player.GetComponent<Health>().TakeDamage(EnemyDamage, Knockback);
+            AttackSwap = true;
+            StartCoroutine(Stun(5));
+        }
     }
 
     IEnumerator Death() {
+        Agent.enabled = false;
         yield return new WaitForSeconds(3.75f);
         Destroy(gameObject);
+    }
+
+    IEnumerator Stun(float duration) {
+        IsStunned = true;
+        yield return new WaitForSeconds(duration);
+        IsStunned = false;
     }
 }
