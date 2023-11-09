67f7ee878ae7f33a3dcf5c10f479aa88f37b9950
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-11-09 00:42:38 -0700

    Scriptable Objects
    Did some scriptable object stuff.

---
+++ Capstone/Assets/ScriptableObjects/ItemScriptableObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemScriptableObject", menuName = "ScriptableObjects/ItemScriptableObject")]
+public class ItemScriptableObject : ScriptableObject {
+    public string ItemName;
+    public string ItemDesc;
+    public Sprite ItemIcon;
+    public int ItemCost;
+
+    public float ItemHealth;
+    public float ItemMana;
+    public float ItemManaRegen;
+    public float ItemCDR;
+    public float ItemPower;
+    public float ItemProtections;
+    public float ItemAS;
+    public float ItemMS;
+    public float ItemPen;
+    public float ItemPercentPen;
+    public float ItemLifesteal;
+}
