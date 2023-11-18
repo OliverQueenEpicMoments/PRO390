d530f4627b74c0b43de2e85292e7de6f20456d91
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-11-18 03:10:08 -0700

    Room Prefab Adjustments
    Further adjusting the room prefabs. Rans into some navmesh trouble as well.

--- Capstone/Assets/Scripts/Rooms/Destroyer.cs
+++ Capstone/Assets/Scripts/Rooms/Destroyer.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (!collision.CompareTag("Player") && !collision.CompareTag("Enemy") && !collision.CompareTag("Confiner") && !collision.CompareTag("Merchant")) {
+        if (!collision.CompareTag("Player") && !collision.CompareTag("Enemy") && !collision.CompareTag("Confiner") && !collision.CompareTag("Merchant") && !collision.CompareTag("NoDestroy")) {
             Destroy(collision.gameObject);
             Debug.Log("Destroyed " + collision);
         }
